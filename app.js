41c2c22a2f3c8a1935cea84830dfe4805665d814
tukkedd <jeamki2501@gmail.com>
2024-09-27 23:39:44 -0400

    refactoring

--- app.js
+++ app.js
@@ -1,20 +1,16 @@
 import express, { json } from 'express'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
 
+import { MovieModel } from "./models/mysql/movie.js";
 
-// import { readJSON } from './utils.js'
-
-// import { createRequire } from 'node:module'
-// const require = createRequire(import.meta.url)
-// const movies = require('./movies.json')
 
 const app = express()
 app.use(json())
 app.use(corsMiddleware())
 app.disable('x-powered-by')
 
-app.use('/movies', moviesRouter)
+app.use('/movies', createMovieRouter({ movieModel: MovieModel }))
 
 const PORT = process.env.PORT ?? 1234
 
--- routes/movies.js
+++ routes/movies.js
@@ -2,11 +2,18 @@
 import { Router } from "express";
 import { MovieController } from "../controllers/movies.js";
 
-export const moviesRouter = Router()
 
-moviesRouter.get('/', MovieController.getAll)
-moviesRouter.post('/', MovieController.create)
+export const createMovieRouter = ({ movieModel }) => {
+    const moviesRouter = Router()
 
-moviesRouter.get('/:id', MovieController.getById)
-moviesRouter.delete('/:id', MovieController.delete)
-moviesRouter.patch('/:id', MovieController.update)
+    const movieController = new MovieController({ movieModel })
+
+    moviesRouter.get('/', movieController.getAll)
+    moviesRouter.post('/', movieController.create)
+
+    moviesRouter.get('/:id', movieController.getById)
+    moviesRouter.delete('/:id', movieController.delete)
+    moviesRouter.patch('/:id', movieController.update)
+
+    return moviesRouter
+}
